9e70d6833a258e6a715d6527fbf19e247567b2d1
shaik-adnan1 <yousufwrk0@gmail.com>
2023-09-11 11:33:03 +0530

    learning node.js inital commit

---
+++ helo.js
@@ -0,0 +1,12 @@
+// const mission = process.argv[2];
+
+// if (mission == "learn") {
+//     console.log('time to write Node code');
+// } else {
+//     console.log(`is ${mission} really more fun?`)
+// }
+
+// Async callbacks
+
+console.log('🐇 finishes')
+console.log('🐢 finishes')
