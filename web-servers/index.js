693b08d782713814b5b35c140b6663ff49198564
shaik-adnan1 <yousufwrk0@gmail.com>
2023-10-10 22:07:59 +0530

    a

--- web-servers/index.js
+++ web-servers/index.js
@@ -33,12 +33,18 @@ const server = http.createServer((req, res) => {
 
     const items = req.url.split('/')
 
-    if(items[1] === 'friends') {
+    if(req.method === 'GET' && items[1] === 'friends') {
         res.writeHead(200, {
             'Content-Type': 'application/json'
         });
 
-        if(items.length === 3) {
+      if(req.method === 'POST' && items[1] === 'friends') {
+        req.on('data', (data) => {
+          const friend = data.toString();
+          console.log('Request: ',  friend);
+          friends.push(JSON.parse(friend));
+        })
+      }else if(items.length === 3) {
             res.end(JSON.stringify(friends[Number(items[2])]))
         } else {
 
