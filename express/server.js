f62c2ca9783241c3b11d168f0c22a9d082979d31
shaik-adnan1 <yousufwrk0@gmail.com>
2023-10-11 13:39:51 +0530

    implemented parameterized routes

--- express/server.js
+++ express/server.js
@@ -4,15 +4,59 @@ const app = express();
 
 const PORT = 3000;
 
+const friends = [
+  {
+    id: 0,
+    name: "Oggy",
+  },
+  {
+    id: 1,
+    name: "Sir Issac Newton",
+  },
+  {
+    id: 2,
+    name: "jamal part 2",
+  },
+  {
+    id: 3,
+    name: "Kashimati",
+  },
+  {
+    id: 4,
+    name: "jamal",
+  },
+  {
+    id: 5,
+    name: "issac New to town",
+  },
+];
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
 
 app.get("/", (req, res) => {
   res.send({
-    id: 1, 
-    name: 'Sir Issac New to town'
+    id: 1,
+    name: "Sir Issac New to town",
   });
+});
+
+app.get("/friends", (req, res) => {
+  res.json(friends);
+});
+
+app.get("/friends/:id", (req, res) => {
+  const friendId = Number(req.params.id);
+
+  const friend = friends[friendId];
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({
+        error: 'friend does not exist'
+    })
+  }
 });
 
 app.get("/messages", (req, res) => {
